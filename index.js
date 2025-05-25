3dfc541ba266d767b544d3711c695397b524d145
muhammad-adnan <mohammadadnanwasti@gmail.com>
2025-05-25 08:09:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors= require('cors')
+const port =process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('Coffee is getting cold')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
